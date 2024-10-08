0cae12a220244f365f116f15db06ad87956142bd
yuriy05 <dilayyura@gmail.com>
2024-10-08 22:44:25 +0300

    tailwind typography styles

--- src/ui/Header.jsx
+++ src/ui/Header.jsx
@@ -1,12 +1,16 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import SearchOrder from "../features/Order/SearchOrder";
+import Username from "../features/User/Username";
 
 function Header() {
   return (
-    <header>
-      <Link to="/">Fast React Pizza Co.</Link>
+    <header className="bg-yellow-500 uppercase">
+      <Link to="/" className="tracking-widest">
+        Fast React Pizza Co.
+      </Link>
       <SearchOrder />
+      <Username />
     </header>
   );
 }
